1c1d50641b21172b7ff5a50ebbf4759d6443f060
Florian Aumeier <au.flo@protonmail.ch>
2017-01-21 12:33:18 +0100

    Added CollapsingToolbarLayout

--- app/src/main/java/de/flo_aumeier/popularmoviesstage1/MovieActivity.java
+++ app/src/main/java/de/flo_aumeier/popularmoviesstage1/MovieActivity.java
@@ -1,7 +1,12 @@
 package de.flo_aumeier.popularmoviesstage1;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MovieActivity extends AppCompatActivity {
 
