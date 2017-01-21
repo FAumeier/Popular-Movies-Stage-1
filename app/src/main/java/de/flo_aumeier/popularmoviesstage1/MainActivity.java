97be0c65784607919d99fe7b06caba543d52554d
Florian Aumeier <au.flo@protonmail.ch>
2017-01-21 11:57:48 +0100

    Added Menu with Refresh button.

--- app/src/main/java/de/flo_aumeier/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/de/flo_aumeier/popularmoviesstage1/MainActivity.java
@@ -1,7 +1,11 @@
 package de.flo_aumeier.popularmoviesstage1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //TODO: Handle Options Menu
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_refresh) {
+            Context context = MainActivity.this;
+            String textToShow = "Refresh clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
